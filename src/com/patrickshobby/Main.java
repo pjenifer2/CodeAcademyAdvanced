b9c30d32a0f0f4ee5a611ce00bcd4a409fff4779
pjenifer2 <pjuofa95@gmail.com>
2022-04-10 21:29:09 -0700

    add phone number decoder

--- src/com/patrickshobby/Main.java
+++ src/com/patrickshobby/Main.java
@@ -7,6 +7,7 @@ https://www.codecademy.com/resources/blog/advanced-java-code-challenges/
 package com.patrickshobby;
 
 import java.util.ArrayList;
+import java.util.Locale;
 
 public class Main {
 
@@ -19,8 +20,59 @@ public class Main {
         int[] arr = new int[] {10,11,12,13,14,15,16,17,18,19};
         System.out.println(determinePrime(arr));
 
+        //write a phone number decoder
+        String phoneNumber = "800888TEST";
+        System.out.println(phoneNumbers(phoneNumber));
 
 
+
+    }
+
+    /*
+    6. Write a phone number word decoder
+    Write a program in Java that’ll accept a phone number
+    with letters and convert it to a phone number with
+    only digits. For example, if you sent the program a
+    phone number like 800888TEST, it should return (800) 888-8378.
+
+    Note that it shouldn’t only convert letters to the digits they
+    represent but also format the number correctly with parentheses
+    and dashes. And, if you send the program an invalid number,
+    it should throw an error.
+     */
+    private static String phoneNumbers(String phoneNumber) {
+        phoneNumber = phoneNumber.toLowerCase(Locale.ROOT);
+        char[] phoneDigits = new char[10];
+        for (int i = 0; i < phoneNumber.length(); i++){
+            char currentNumber = phoneNumber.charAt(i);
+            if (Character.isDigit(currentNumber)){
+                phoneDigits[i] = currentNumber;
+            } else if (currentNumber == 'a' || currentNumber == 'b' || currentNumber == 'c') {
+                phoneDigits[i] = '2';
+            } else if (currentNumber == 'd' || currentNumber == 'e' || currentNumber == 'f') {
+                phoneDigits[i] = '3';
+            } else if (currentNumber == 'g' || currentNumber == 'h' || currentNumber == 'i') {
+                phoneDigits[i] = '4';
+            } else if (currentNumber == 'j' || currentNumber == 'k' || currentNumber == 'l') {
+                phoneDigits[i] = '5';
+            } else if (currentNumber == 'm' || currentNumber == 'n' || currentNumber == 'o') {
+                phoneDigits[i] = '6';
+            } else if (currentNumber == 'p' || currentNumber == 'q' || currentNumber == 'r' || currentNumber == 's') {
+                phoneDigits[i] = '7';
+            } else if (currentNumber == 't' || currentNumber == 'u' || currentNumber == 'v') {
+                phoneDigits[i] = '8';
+            } else {
+                phoneDigits[i] = '9';
+            }
+
+        }
+
+        String areaCode = "" + phoneDigits[0] + phoneDigits[1] + phoneDigits[2] + "";
+        String firstThree = "" + phoneDigits[3] + phoneDigits[4] + phoneDigits[5] +"";
+        String lastFour = "" + phoneDigits[6] + phoneDigits[7] + phoneDigits[8] + phoneDigits[9];
+        String digitsToDial = "(" + areaCode + ") " + firstThree + "-" + lastFour;
+
+        return digitsToDial;
     }
 
     /*
